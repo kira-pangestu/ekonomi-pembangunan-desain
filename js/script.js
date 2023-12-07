8b4fb46bc59cf6ba70a7e89ff58a11efbb49daf9
kira-pangestu <140051123+kira-pangestu@users.noreply.github.com>
2023-12-07 09:52:53 +0900

    update terbaru

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// toggle class active
+// toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika humburger menu di klik
@@ -7,10 +7,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidbar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searhBox = document.querySelector("#search-box");
+
+document.querySelector("#search-botton").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searhBox.focus();
+  e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-botton");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
